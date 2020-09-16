3689c9135ed844b99d6d476ceaf113c4f89d662b
Jauhen Feldman <feldmeow@gmail.com>
2020-09-16 16:26:13 +0300

    Edit code & add comments

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ const todoControl = document.querySelector('.todo-control'),
 
 let todoData = [];
 
-
+// Валидация(проверка на пустой инпут)
 function isEmpty() {
   if (headerInput.value.replace(/\s/g, '').length > 0) {
     return true;
@@ -16,6 +16,7 @@ function isEmpty() {
   }
 };
 
+// чтение из localStorage
 function readLocalStorage() {
   if (localStorage.todoData) {
     return todoData = JSON.parse(localStorage.todoData);
@@ -40,12 +41,15 @@ const render = function () {
       todoList.append(li);
     }
 
+    // Отметка о выполненной/не выполненной задаче
     const btnComplete = li.querySelector('.todo-complete');
     btnComplete.addEventListener('click', function () {
       item.completed = !item.completed;
       render();
 
     });
+
+    // удаление объекта из todoData и обновление данных
     const btnDelete = li.querySelector('.todo-remove');
     btnDelete.addEventListener('click', function (event) {
       for (let i = 0; i <= todoData.length; i++) {
@@ -58,12 +62,14 @@ const render = function () {
     });
 
   });
+  // Сохранение в localStorage + конвертация в JSON
   function toJsonLocalStorage() {
     return localStorage.todoData = JSON.stringify(todoData);
   };
   toJsonLocalStorage();
 };
 
+// добавление новой задачи в массив(чтение из инпута) 
 todoControl.addEventListener('submit', function (event) {
   isEmpty();
   event.preventDefault();
